e4a4d7e51767733c316971b7c4baed179b5d1ea4
nirnts <nirnts@gmail.com>
2020-12-24 19:48:24 +0200

    Finished taxi graph

--- multitaxienv/taxi_wrapper.py
+++ multitaxienv/taxi_wrapper.py
@@ -1,29 +1,9 @@
 # In this file we will implement the taxi wrapper
 import networkx as nx
 
-MAP1 = [
-    "+---------+",
-    "|X: |F: :X|",
-    "| : | : : |",
-    "| : : : : |",
-    "| | : | : |",
-    "|X| :G|X: |",
-    "+---------+",
-]
-
-MAP2 = [
-    "+-----------+",
-    "|X: |F: :X: |",
-    "| : | : : : |",
-    "| : : : : : |",
-    "| | : | : : |",
-    "|X| :G|X: : |",
-    "+-----------+",
-]
-
 
 class TaxiMap:
-    def __init__(self, desc):
+    def __init__(self, desc : list):
         """
         Args:
             desc: Map description (list of strings)
@@ -33,19 +13,35 @@ class TaxiMap:
         self.graph = nx.empty_graph(self.rows * self.cols)
         for i in self.graph.nodes:
             row, col = self.node_to_cors(i)
-            # if desc[row][col * 2 + 1] != '-':  # Check north
-            #     self.graph.add_edge(i, self.cors_to_node(row - 1, col))
             if desc[row + 2][col * 2 + 1] != '-':  # Check south
                 self.graph.add_edge(i, self.cors_to_node(row + 1, col))
-            # if desc[row + 1][col * 2] == ':':  # Check west
-            #     self.graph.add_edge(i, self.cors_to_node(row, col - 1))
+                # In case we ever use horizontal barriers
             if desc[row + 1][col * 2 + 2] == ':':  # Check east
                 self.graph.add_edge(i, self.cors_to_node(row, col + 1))
-        print('hi')
 
-    def node_to_cors(self, node):
+    def node_to_cors(self, node) -> (int, int):
         return node // self.cols, node % self.cols
 
-    def cors_to_node(self, row, col):
+    def cors_to_node(self, row, col) -> int:
         return row * self.cols + col
+
+    def get_path(self, origin: (int, int), target: (int, int)):
+        node_origin, node_target = self.cors_to_node(*origin), self.cors_to_node(*target)
+        if node_origin == node_target:
+            return [origin], []
+
+        path = nx.shortest_path(self.graph, node_origin, node_target)
+        cord_path = [self.node_to_cors(node) for node in path]
+        actions = []
+        for node in range(len(path) - 1):
+            delta = path[node + 1] - path[node]
+            if delta == -1:  # West
+                actions.append(3)
+            elif delta == 1:  # East
+                actions.append(2)
+            elif delta == -self.cols:  # North
+                actions.append(1)
+            else:  # South
+                actions.append(0)
+        return cord_path, actions
 
